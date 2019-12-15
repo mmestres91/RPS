42e3a545fc93b187309b5c6ed0970839b76259bd
Mark Mestres <mmestres91@gmail.com>
2019-12-15 00:45:03 -0500

    Update RPS.py
    Added player subclasses, scoring method and scenario for win/loss

--- RPS.py
+++ RPS.py
@@ -1,12 +1,7 @@
 import random
 
-"""This program plays a game of Rock, Paper, Scissors between two Players,
-and reports both Player's scores each round."""
-
 moves = ['rock', 'paper', 'scissors']
 
-"""The Player class is the parent class for all of the Players
-in this game"""
 
 class Player:
     def move(self):
@@ -22,7 +17,19 @@ class RandomPlayer(Player):
 
 class HumanPlayer(Player):
     def move(self):
-      input("Please enter your move!")
+        move = input("Please enter your move!")
+        if move in moves:
+            return move
+        else: 
+            HumanPlayer.move(self)
+
+class ReflectPlayer(HumanPlayer):
+    def move(self):
+        return HumanPlayer.move(self)
+
+class CyclePlayer(Player):
+    def move(self):
+        return "rock"
 
 
 def beats(one, two):
@@ -31,35 +38,37 @@ def beats(one, two):
             (one == 'paper' and two == 'rock'))
 
 
-
 class Game:
+    player1score = 0 
+    player2score = 0
     def __init__(self, p1, p2):
         self.HumanPlayer = p1
         self.RandomPlayer = p2
 
-    def play_round(self, HumanPlayer, RandomPlayer, player1score, player2score):
-        move1 = self.HumanPlayer()
+    def play_round(self, HumanPlayer, RandomPlayer):
+        move1 = self.HumanPlayer.move()
         move2 = self.RandomPlayer.move()
-        print(f"Player 1: {move1}  Player 2: {move2}")
+        print(f"Player One: {move1}  Player Two: {move2}")
         if beats(move1, move2) is True:
-            player1score += 1
-            print("Player 1 is the winner!")
+            self.player1score += 1
         elif beats(move2, move1) is True: 
-            player2score += 1
-            print("Player 2 is the winner!")
+            self.player2score += 1
         else:
-            print("We have a tie!")
-        print(f"Score: Player 1: {player1score} Player 2: {player2score}")
-        self.RandomPlayer.learn(move1, move2)
-        self.RandomPlayer.learn(move2, move1)     
+            print("TIE! No points awarded")
+        print(f"Score: Player One: {self.player1score} Player Two: {self.player2score}")
+        self.RandomPlayer.learn(move1, move2)    
 
     def play_game(self):
-        player1score = 0 
-        player2score = 0
         print("Game start!")
-        for round in range(6):
+        for round in range(5):
             print(f"Round {round}:")
-            self.play_round(HumanPlayer, RandomPlayer, player1score, player2score)
+            self.play_round(HumanPlayer, RandomPlayer)
+        if self.player1score > self.player2score:
+            print("You Won!")
+        elif self.player1score == self.player2score:
+            print("It's a Final Tie!")
+        else:
+            print("You Lost!")
         print("Game over!")
 
 
