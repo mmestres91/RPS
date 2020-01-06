95328220b445b322ca56d243e7d41c7457b7bedc
Mark Mestres <mmestres91@gmail.com>
2020-01-05 20:34:50 -0500

    Flask App Updates

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+import RPS
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    greeting = "Hello World"
+    return render_template("index.html", greeting=greeting)
+
+if __name__ == "__main__":
+     app.run()
